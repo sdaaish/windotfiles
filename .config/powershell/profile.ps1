f0cab34d2cdc0d88f7672239a181b28efe03bc0c
Stig Dahl <stig@charlottendal.net>
2022-01-22 23:15:36 +0100

    Fix git prompt with posh-git

--- .config/powershell/profile.ps1
+++ .config/powershell/profile.ps1
@@ -16,11 +16,15 @@ Set-PSReadlineOption @PSReadLineOptions
 # Add output of all commands to $__, set as default value
 $PSDefaultParameterValues["Out-Default:OutVariable"] = "__"
 
+
 # Oh-my-PoSH https://ohmyposh.dev/docs
 $Theme = Join-Path ${env:USERPROFILE} ".config\oh-my-posh\my-posh-theme.omp.json"
+$env:POSH_GIT_ENABLE = $true
+Import-Module Posh-Git
 oh-my-posh.exe --init --shell pwsh --config $Theme | Invoke-Expression
 Enable-PoshTransientPrompt
 
+# Colorthemes for files
 $ColorTheme = Join-Path ${env:UserProfile} ".config\ColorThemes\MyColorTheme.psd1"
 Add-TerminalIconsColorTheme -Path $ColorTheme -Force
 Set-TerminalIconsTheme -ColorTheme MyColorTheme
