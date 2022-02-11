89e51e65f4b1296214a822643623829a3cd4ad27
Stig Dahl <stig@charlottendal.net>
2022-02-11 10:21:02 +0100

    Update with some packages

--- .config/emacs.lsp/emacs-custom.el
+++ .config/emacs.lsp/emacs-custom.el
@@ -6,7 +6,7 @@
  '(counsel-mode t)
  '(custom-enabled-themes '(material))
  '(custom-safe-themes
-   '("7922b14d8971cce37ddb5e487dbc18da5444c47f766178e5a4e72f90437c0711" default))
+   '("42186ee267235ca4b44b61569eb0624483dcbf0bb5cf1c46881cd248362e8e9b" "bdb7d74f17ecd6cdf47f4f4710df40fb10a8ed5edf829783738827d09b1fca89" "90a6f96a4665a6a56e36dec873a15cbedf761c51ec08dd993d6604e32dd45940" "7922b14d8971cce37ddb5e487dbc18da5444c47f766178e5a4e72f90437c0711" default))
  '(electric-pair-mode t)
  '(explicit-powershell\.exe-args '("-NoProfile" "-Command" "-")))
 (custom-set-faces
