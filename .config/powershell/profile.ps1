2181f7e951fbb44d880bd53d66eccf057cb20015
Stig Dahl <stig@charlottendal.net>
2022-02-15 12:19:25 +0100

    Use starship instead of oh-my-posh for the prompt for performance

--- .config/powershell/profile.ps1
+++ .config/powershell/profile.ps1
@@ -1,6 +1,6 @@
 # Local version of Powershell profile
-
 Import-Module $PSScriptRoot\MyFunctions\MyFunctions\MyFunctions.psd1
+Get-RunningTime $starttime
 
 # Use a local path for modules
 $env:PSModulePath = Set-LocalModulePath
@@ -12,6 +12,7 @@ $PSReadLineOptions = @{
     PredictionSource = "History"
 }
 Set-PSReadlineOption @PSReadLineOptions
+Get-RunningTime $starttime
 
 # Add output of all commands to $__, set as default value
 $PSDefaultParameterValues["Out-Default:OutVariable"] = "__"
@@ -21,13 +22,19 @@ $PSDefaultParameterValues["Out-Default:OutVariable"] = "__"
 $Theme = Join-Path ${env:USERPROFILE} ".config\oh-my-posh\my-posh-theme.omp.json"
 $env:POSH_GIT_ENABLED = $true
 Import-Module Posh-Git
-oh-my-posh.exe --init --shell pwsh --config $Theme | Invoke-Expression
-Enable-PoshTransientPrompt
+Get-RunningTime $starttime
+
+#oh-my-posh.exe --init --shell pwsh --config jandedobbeleer | Invoke-Expression
+#Enable-PoshTransientPrompt
+$ENV:STARSHIP_CONFIG = Join-Path ${env:UserProfile} ".config\starship\starship.toml"
+Invoke-Expression (&starship init powershell)
+Get-RunningTime $starttime
 
 # Colorthemes for files
 $ColorTheme = Join-Path ${env:UserProfile} ".config\ColorThemes\MyColorTheme.psd1"
 Add-TerminalIconsColorTheme -Path $ColorTheme -Force
 Set-TerminalIconsTheme -ColorTheme MyColorTheme
+Get-RunningTime $starttime
 
 if (Test-Path $PSScriptRoot\aliases.ps1){
     . $PSScriptRoot\aliases.ps1
