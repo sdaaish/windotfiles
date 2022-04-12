57a45523121f70cf098707f89478060fd5a8ddf2
Stig Dahl <stig@charlottendal.net>
2022-04-12 10:30:45 +0200

    Use listview for predictions

--- .config/powershell/profile.ps1
+++ .config/powershell/profile.ps1
@@ -14,6 +14,7 @@ $PSReadLineOptions = @{
     EditMode = "Emacs"
     BellStyle = "None"
     PredictionSource = "History"
+    PredictionViewStyle = "ListView"
 }
 Set-PSReadlineOption @PSReadLineOptions
 Get-RunningTime $starttime
